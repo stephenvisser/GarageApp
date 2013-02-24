78058292b4aca6b74f535868bec114b4fe62e40c
Stephen Visser <visser.stephen@gmail.com>
2013-02-24 13:47:09 -0700

    Added the ability to send a request when a swipe occurs

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,9 @@ var img = document.getElementById('door');
 Hammer(document.body)
 	.on('swipeup', function(event){
 		img.classList.add('open');
+		$.get('http://visserpi:8000');
 	})
 	.on('swipedown', function(event) {
 		img.classList.remove('open');
+		$.get('http://visserpi:8000');
 	});
